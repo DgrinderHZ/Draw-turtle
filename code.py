71c98610bcbb7848e2c24db4a0f97b345db1ea4b
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-03 01:45:20 +0000

    Add draw-triangle

--- code.py
+++ code.py
@@ -28,8 +28,18 @@ def draw_circle():
 	pen.color("green")
 	pen.circle(100)
 
+def draw_triangle():
+	donatilo = turtle.Turtle()
+	donatilo.shape("turtle")
+	donatilo.color("violet")
+	donatilo.speed(10)
+	for i in range(0,3):
+		donatilo.forward(200)
+		donatilo.left(120)
+
 window = turtle.Screen()
 window.bgcolor("red")
 draw_square()
 draw_circle()
+draw_triangle()
 window.exitonclick()
