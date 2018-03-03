f6c0cb2cac93254506aaccc543a54950cccd8ed8
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-03 22:35:44 +0000

    Add draw_circle_out_of_square

--- code.py
+++ code.py
@@ -33,13 +33,30 @@ def draw_triangle():
 	donatilo.shape("turtle")
 	donatilo.color("violet")
 	donatilo.speed(10)
+	donatilo.left(180)
 	for i in range(0,3):
 		donatilo.forward(200)
-		donatilo.left(120)
+		donatilo.right(120)
+
+def draw_circle_out_of_square():
+	pencil = turtle.Turtle()
+	# Costumizing section!
+	pencil.shape("turtle")
+	pencil.color("pink")
+	pencil.speed(20)
+	pencil.right(140)
+	pencil.forward(200)
+
+	for i in range(0,72):
+		for x in range(0,4):
+			pencil.forward(80)
+			pencil.right(90)
+		pencil.left(5)
 
 window = turtle.Screen()
 window.bgcolor("red")
 draw_square()
 draw_circle()
 draw_triangle()
+draw_circle_out_of_square()
 window.exitonclick()
